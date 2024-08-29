8658e06fd598403e6827646e0b3713c750d1457a
quentin <soranzokrebsquentin@gmail.com>
2024-08-29 10:11:18 +0200

    starting add cmake

--- src/perso.h
+++ src/perso.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2024 Quentin Soranzo Krebs
+ *
+ * This file is part of Pac-Wall.
+ *
+ * [Nom de ton projet] is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pac-Wall is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pac-Wall. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 #ifndef PERSO_H
 #define PERSO_H
 
